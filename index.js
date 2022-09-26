31a82351c5d1b7f57c7ca80da4818837b95d04a8
kev-Fuentes <kfuentes@condorlabs.io>
2022-09-26 15:21:28 -0500

    fixed solve

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const axios = require("axios").default;
-const moment = require("moment");
-const fs = require("fs");
-const LIMIT_EMPLOYEE = 1;
+const axios = require('axios').default;
+const moment = require('moment');
+const fs = require('fs');
+const LIMIT_EMPLOYEE = 19;
 const POSTHIRE_API = axios.create({
-  baseURL: "https://posthire.evercheck.com",
-  timeout: 1000,
+  baseURL: 'https://posthire.evercheck.com',
 });
 
-const parsedDate = (date, formatDate) => {
-  return moment(date).format(formatDate);
+const parsedDate = (date, formatDate, utc = false) => {
+  return utc
+    ? moment(date).utc().format(formatDate)
+    : moment(date).format(formatDate);
 };
 
 const getEmployee = async (employeeId) => {
@@ -17,6 +18,7 @@ const getEmployee = async (employeeId) => {
   );
 
   return employees.data.map((employee) => {
+    console.log(employee);
     const parsedEmployee = {
       employerId: employee.employer_id,
       totalItems: employee.total_items,
@@ -24,25 +26,26 @@ const getEmployee = async (employeeId) => {
       employeeCountAfter: employee.employee_count_after,
       status: employee.status,
       ftp: employee.is_ftp,
-      startDate: parsedDate(employee.created_at, "YYYY-MM-DD"),
-      startDateAndTime: parsedDate(employee.created_at, "YYYY-MM-DD HH:mm:ss"),
-      finishDate: parsedDate(employee.finished_at, "YYYY-MM-DD"),
+      startDate: parsedDate(employee.created_at, 'YYYY-MM-DD', true),
+      startDateAndTime: parsedDate(
+        employee.created_at,
+        'YYYY-MM-DD HH:mm:ss',
+        true
+      ),
+      finishDate: parsedDate(employee.finished_at, 'YYYY-MM-DD', true),
       finishDateAndTime: parsedDate(
         employee.finished_at,
-        "YYYY-MM-DD HH:mm:ss"
+        'YYYY-MM-DD HH:mm:ss',
+        true
       ),
-      completion: "100%",
+      completion: '100%',
     };
     return parsedEmployee;
   });
 };
 
 const start = async () => {
-  const employees = await Promise.all([
-    getEmployee(640),
-    getEmployee(559),
-    getEmployee(667),
-  ]);
+  const employees = await Promise.all([getEmployee(719)]);
   const flatArrayEmployees = employees.flat(Infinity);
 
   const orderEmployees = flatArrayEmployees.sort(
@@ -53,10 +56,10 @@ const start = async () => {
 
   const strOrderEmployees = JSON.stringify(orderEmployees);
 
-  fs.writeFile("employee.txt", strOrderEmployees, (err) => {
+  fs.writeFile('employee.txt', strOrderEmployees, (err) => {
     if (err) throw err;
 
-    console.log("Employee saved!");
+    console.log('Employee saved!');
   });
 };
 
